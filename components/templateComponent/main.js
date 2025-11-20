945ff3684b576ab81f4fcfd23d34b4eab875a2cc
Kinga Janicka <kingajanicka7@gmail.com>
2025-11-20 11:27:18 +0000

    scaffolded first component

---
+++ components/templateComponent/main.js
@@ -0,0 +1,30 @@
+class AdvancedTooltip extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' }); // Enables Shadow DOM for encapsulation
+    }
+
+    connectedCallback() {
+        const text = this.getAttribute('text');
+        this.shadowRoot.innerHTML = `
+      <style>
+        .tooltip {
+            background-color:blue;
+        }
+        .tooltip-text {
+            visibility: hidden;
+        }
+        .tooltip:hover {
+            visibility: visible;
+            background-color:yellow;
+        }
+      </style>
+      <div class="tooltip">
+        <slot></slot>
+        <span class="tooltip-text">${text}</span>
+      </div>
+    `;
+    }
+}
+
+customElements.define('advanced-tooltip', AdvancedTooltip);
