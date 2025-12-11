9a8319797f209e7155cba3ff3f9b943ba35ceb48
Kinga Janicka <kingajanicka7@gmail.com>
2025-12-11 18:55:28 +0000

    Path fix for deployment

--- .svelte-kit/generated/client/nodes/5.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../src/routes/our-story/+page.svelte";
--- svelte.config.js
+++ svelte.config.js
@@ -12,5 +12,8 @@ export default {
       // precompress: false,
       // strict: true
     })
+  },
+  paths: {
+    base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
   }
 }
