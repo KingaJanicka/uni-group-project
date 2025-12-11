610684e19daf2c680caf65b563d301907d07c64b
Kinga Janicka <kingajanicka7@gmail.com>
2025-12-11 18:59:47 +0000

    Missing config opt

--- svelte.config.js
+++ svelte.config.js
@@ -11,9 +11,9 @@ export default {
       // fallback: undefined,
       // precompress: false,
       // strict: true
-    })
+    }),
+    paths: {
+      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+    }
   },
-  paths: {
-    base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-  }
 }
